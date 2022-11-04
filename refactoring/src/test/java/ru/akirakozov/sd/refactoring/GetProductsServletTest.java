836138016d87f92221b2558c437d18b83753a3e2
mk17ru <mk17.ru@mail.ru>
2022-11-05 02:13:53 +0300

    add refactoring HW3

---
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/GetProductsServletTest.java
@@ -0,0 +1,28 @@
+package ru.akirakozov.sd.refactoring;
+
+import org.junit.jupiter.api.Test;
+import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
+import ru.akirakozov.sd.refactoring.servlet.GetProductsServlet;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+import static org.mockito.Mockito.verify;
+
+public class GetProductsServletTest extends BaseServletTest {
+
+	@Test
+	public void getProductTest() throws IOException {
+		GetProductsServlet servlet = new GetProductsServlet();
+		initRequestMock(Map.of());
+
+		servlet.doGet(httpServletRequest, httpServletResponse);
+
+		verify(httpServletResponse).setStatus(HttpServletResponse.SC_OK);
+		verify(httpServletResponse.getWriter()).println("<html><body>");
+		verify(httpServletResponse.getWriter()).println("</body></html>");
+	}
+
+
+}
