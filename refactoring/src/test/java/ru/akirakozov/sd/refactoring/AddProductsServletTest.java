073ef891b1e25cafef6a47a0cfd3dea86e906d52
mk17ru <mk17.ru@mail.ru>
2022-11-05 02:22:28 +0300

    add AddProductsServletTest

---
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/AddProductsServletTest.java
@@ -0,0 +1,28 @@
+package ru.akirakozov.sd.refactoring;
+
+import org.junit.jupiter.api.Test;
+import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+import static org.mockito.Mockito.verify;
+
+public class AddProductsServletTest extends BaseServletTest {
+
+	@Test
+	public void addProductTest() throws IOException {
+		AddProductServlet servlet = new AddProductServlet();
+		initRequestMock(Map.of(
+				"name", "shoes",
+				"price", "999"
+		));
+		servlet.doGet(httpServletRequest, httpServletResponse);
+
+		verify(httpServletResponse).setStatus(HttpServletResponse.SC_OK);
+		verify(httpServletResponse.getWriter()).println("OK");
+	}
+
+
+}
